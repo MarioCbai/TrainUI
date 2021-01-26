1cfc79f5f1138fb838ec25f5585f78e59b8230ce
1173090905@qq.com <1173090905@qq.com>
2021-01-26 21:08:57 +0800

    郭

---
+++ TeachingCultureUI/TeachingCultureUI/CheckUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+namespace TeachingCultureUI
+{
+    public class CheckUser : ActionFilterAttribute 
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            int  filter =Convert.ToInt32(context.HttpContext.Session.GetInt32("Rnumber"));
+            if (filter==0)
+            {
+                context.Result = new RedirectResult("/AuthorityM/Register");
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
